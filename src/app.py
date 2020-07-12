8fc61cdc24710be16806a8cd5ddb9ac87885cb67
Alexander Robles <alexander.robles@ibm.com>
2020-07-11 21:23:51 -0300

    Version 0.1.0

--- src/app.py
+++ src/app.py
@@ -12,7 +12,6 @@ warnings.filterwarnings('ignore')
 
 st.title('Time Series Analysis')
 
-@st.cache
 def tsplot(y, lags=None, figsize=(20, 12), style='bmh'):
     """
         Plot time series, its ACF and PACF
@@ -20,9 +19,6 @@ def tsplot(y, lags=None, figsize=(20, 12), style='bmh'):
         y - timeseries
         lags - how many lags to include in ACF, PACF calculation
     """
-    
-    if not isinstance(y, pd.Series):
-        y = pd.Series(y)
         
     with plt.style.context(style):    
         fig = plt.figure(figsize=figsize)
@@ -38,15 +34,43 @@ def tsplot(y, lags=None, figsize=(20, 12), style='bmh'):
 
 def main():
     # As an example, let's look at real mobile game data. Specifically, we will look into ads watched per hour and in-game currency spend per day:
-    ads = pd.read_csv('ads.csv', index_col=['Time'], parse_dates=['Time'])
+    # ads = pd.read_csv('ads.csv', index_col=['Time'], parse_dates=['Time'])
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
     # Add a slider to the sidebar:
     x = st.sidebar.slider(
         'Select a lang',
         50, 60
     )
-    c = tsplot(ads.Ads, lags=x)
-    st.write(ads)
-    st.pyplot()
+    if uploaded_file is not None:
+        data = pd.read_csv(uploaded_file)
+        st.subheader("Exploratory Data Analysis")
+        # Show dataset
+        if st.checkbox("Show Dataset"):
+            rows = st.number_input("Number of rows", 5, len(data))
+            st.dataframe(data.head(rows))
+        # Show columns
+        if st.checkbox("Columns"):
+            st.write(data.columns)
+        # Show Shape
+        if st.checkbox("Shape of Dataset"):
+            data_dim = st.radio("Show by", ("Rows", "Columns", "Shape"))
+            if data_dim == "Columns":
+                st.text("Number of Columns: ")
+                st.write(data.shape[1])
+            elif data_dim == "Rows":
+                st.text("Number of Rows: ")
+                st.write(data.shape[0])
+            else:
+                st.write(data.shape)
+        if st.checkbox("Select column as time series"):
+            columns = data.columns.tolist()
+            selected = st.multiselect("Choose", columns)
+            series = data[selected]
+            if st.button('Plot Time Series, ACF and PACF'):
+                tsplot(series, lags=x)
+                st.pyplot()
+
+
 
 if __name__ == "__main__":
     main()
