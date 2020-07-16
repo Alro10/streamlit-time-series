bef56c3470565f349c5f616a30c91ca5fe6e3351
Alexander Robles <alexander.robles@ibm.com>
2020-07-16 19:54:24 -0300

    Add building step code

--- src/app.py
+++ src/app.py
@@ -6,6 +6,13 @@ import scipy.stats as scs
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+#sktime models
+from sktime.forecasting.model_selection import temporal_train_test_split
+from sktime.performance_metrics.forecasting import smape_loss
+from sktime.utils.plotting.forecasting import plot_ys
+from sktime.forecasting.arima import AutoARIMA
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -49,21 +56,30 @@ def null_values(df):
 def types(df):
     return pd.DataFrame(df.dtypes, columns=['Type'])
 
+def forecasting_autoarima(y_train, y_test, s):
+    fh = np.arange(len(y_test)) + 1
+    forecaster = AutoARIMA(sp=s)
+    forecaster.fit(y_train)
+    y_pred = forecaster.predict(fh)
+    plot_ys(y_train, y_test, y_pred, labels=["y_train", "y_test", "y_pred"])
+    st.pyplot()
+
 def main():
     st.sidebar.title("What to do")
     activities = ["Exploratory Data Analysis", "Plotting and Visualization", "Building Model", "About"]
     choice = st.sidebar.selectbox("Select Activity", activities)
     # Upload file
     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-    # Add a slider to the sidebar:
-    st.sidebar.markdown("# Lang")
-    x = st.sidebar.slider(
-        'Select a lang',
-        50, 60
-    )
-    if uploaded_file is not None:
+
+    if uploaded_file is not None and choice == "Exploratory Data Analysis":
         data = pd.read_csv(uploaded_file)
-        st.subheader("Exploratory Data Analysis")
+        st.subheader(choice)
+        # Add a slider to the sidebar:
+        st.sidebar.markdown("# Lang")
+        x = st.sidebar.slider(
+            'Select a lang',
+            50, 60
+        )
         # Show dataset
         if st.checkbox("Show Dataset"):
             rows = st.number_input("Number of rows", 5, len(data))
@@ -102,7 +118,74 @@ def main():
                 tsplot(series, lags=x)
                 st.pyplot()
 
+    elif uploaded_file is not None and choice == "Plotting and Visualization":
+        st.subheader(choice)
+        data = pd.read_csv(uploaded_file)
+        df = data.copy()
+        all_columns = df.columns.tolist()
+        type_of_plot = st.selectbox("Select Type of Plot", ["area", "line", "scatter", "pie", "bar", "correlation"]) 
+        
+        if type_of_plot=="line":
+            select_columns_to_plot = st.multiselect("Select columns to plot", all_columns)
+            cust_data = df[select_columns_to_plot]
+            st.line_chart(cust_data)
+        
+        elif type_of_plot=="area":
+            select_columns_to_plot = st.multiselect("Select columns to plot", all_columns)
+            cust_data = df[select_columns_to_plot]
+            st.area_chart(cust_data)  
+        
+        elif type_of_plot=="bar":
+            select_columns_to_plot = st.multiselect("Select columns to plot", all_columns)
+            cust_data = df[select_columns_to_plot]
+            st.bar_chart(cust_data)
+        
+        elif type_of_plot=="pie":
+            select_columns_to_plot = st.selectbox("Select a column", all_columns)
+            st.write(df[select_columns_to_plot].value_counts().plot.pie())
+            st.pyplot()
+        
+        elif type_of_plot=="correlation":
+            st.write(sns.heatmap(df.corr(), annot=True, linewidths=.5, annot_kws={"size": 7}))
+            st.pyplot()
 
+        elif type_of_plot=="scatter":
+            st.write("Scatter Plot")
+            scatter_x = st.selectbox("Select a column for X Axis", all_columns)
+            scatter_y = st.selectbox("Select a column for Y Axis", all_columns)
+            st.write(sns.scatterplot(x=scatter_x, y=scatter_y, data = df))
+            st.pyplot()
+
+    elif choice == "Building Model":
+        st.subheader(choice)
+         # Add a slider to the sidebar:
+        st.sidebar.markdown("# Seasonal")
+        s = st.sidebar.slider(
+            'Select a s',
+            24, 48
+        )
+        data = pd.read_csv(uploaded_file)
+        df = data.copy()
+        st.write("Select the columns to use for training")
+        columns = df.columns.tolist()
+        selected_column = st.multiselect("Select Columns", columns)
+        new_df = df[selected_column]
+        st.write(new_df)
+
+        if st.button("Train/Test Split"):
+            y_train, y_test = temporal_train_test_split(new_df.T.iloc[0])
+            st.text("Train Shape")
+            st.write(y_train.shape)
+            st.text("Test Shape")
+            st.write(y_test.shape)
+            plot_ys(y_train, y_test, labels=["y_train", "y_test"])
+            st.pyplot()
+            
+        if st.button("Training a Model"):
+            y_train, y_test = temporal_train_test_split(new_df.T.iloc[0])
+            model_selection = st.selectbox("Model to train", ["AutoArima", "LSTM", "MLP", "RNN"])
+            if model_selection == "AutoArima":
+                forecasting_autoarima(y_train, y_test, s)
 
 if __name__ == "__main__":
     main()
